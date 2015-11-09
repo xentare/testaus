4e2e0c842effe112e235032bea3206f1ef7fd29f
xentare <juha.puskala@gmail.com>
2015-11-09 15:03:17 +0200

    asd

---
+++ src/Valuuttakone/Testaus.java
@@ -0,0 +1,11 @@
+package Valuuttakone;
+public class Testaus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
